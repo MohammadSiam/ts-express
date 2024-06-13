59b1528035d226c90be5fa5718fd3c0102d1ec4a
Siam <siamk417@gmail.com>
2024-06-13 12:34:18 +0600

    Update config.ts

--- src/db/config.ts
+++ src/db/config.ts
@@ -6,11 +6,11 @@ import { Todos } from "../models/todos.model";
 
 const connectionOptions: ConnectionOptions = {
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "",
-  database: "login_system",
+  host: "mysql-18acf246-siamk417-ca09.j.aivencloud.com",
+  port: 14477,
+  username: "avnadmin",
+  password: "AVNS_EX5WddA-zAGScjrhpp2",
+  database: "defaultdb",
   entities: [Todos, Registration, Login, BookMeeting],
   synchronize: true,
   logging: false,
